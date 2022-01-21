acff25006ff1777bca252cb11a650008286fd8bc
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-01-20 19:57:44 -0800

    Done with reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector('input#searchByID')
+    console.log(input.value)
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+      const title = document.querySelector('h4')
+      title.textContent = data.title
+      const summary = document.querySelector('section#movieDetails p')
+      summary.textContent = data.summary
+    })
+    inputForm.reset()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
